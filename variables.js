929bb55a0c789057d94f8bbc763cc880ae1ee61b
Jiří Klimeš <it.klimes@gmail.com>
2022-09-13 17:28:06 +0200

    Created and edited variables.js practice file

---
+++ variables.js
@@ -0,0 +1,48 @@
+// STRINGS
+
+let firstName = "George"
+
+let secondName = "Klimes"
+
+let city = "Brno"
+
+let job = "student"
+
+// NUMBERS
+
+let age = 23
+
+let decimal = 10.
+
+let decade = 10
+
+console.log("Hello, my name is " + firstName + " " + secondName + ", I am " + age + " years old. I live in  " + city + " and I am a/an " + job)
+
+// SOME RULES
+
+// Variable can be declared only once
+
+let number = 20
+
+/*
+Cant initialize the value again
+
+let number = 25
+
+Can only reassign its value
+*/
+
+number = 15
+
+let _underline = "Can start with underline"
+
+let $dollarSign = "Can start with dollar sign"
+
+/*
+let &andSing = "Can't start with other symbols"
+*/
+
+/*
+let 5number = "Can't start with numbers"
+*/
+
