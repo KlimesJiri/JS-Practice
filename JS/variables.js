9d40d7ff2fb4ae627fde4bff6fcaff989b4d5b3c
Jiří Klimeš <it.klimes@gmail.com>
2022-09-15 13:26:28 +0200

    Added JS operators cheat sheet

--- JS/variables.js
+++ JS/variables.js
@@ -1,4 +1,4 @@
-// STRINGS
+// **  STRINGS  **
 
 let firstName = "George"
 
@@ -8,7 +8,10 @@ let city = "Brno"
 
 let job = "student"
 
-// NUMBERS
+
+
+
+// **  NUMBERS  **
 
 let age = 23
 
@@ -17,6 +20,9 @@ let decimal = 10.
 let decade = 10
 
 console.log("Hello, my name is " + firstName + " " + secondName + ", I am " + age + " years old. I live in  " + city + " and I am a/an " + job)
+
+
+
 
 // SOME RULES
 
@@ -46,3 +52,58 @@ let &andSing = "Can't start with other symbols"
 let 5number = "Can't start with numbers"
 */
 
+
+
+
+// **  BOOLEAN  **
+
+let married = false
+
+let adult = true
+
+
+
+
+// **  OPERATORS  **
+
+// EXAMPLES
+// assign value =
+let x = 10
+
+// addition
+let y = x + 5
+
+//multiplication
+let z = 5 * y
+
+// List of all the operators
+/*
+    + ADDITION
+    - SUBTRACTION
+    * MULTIPLICATION
+    **EXPONENTIATION
+    / DIVISION
+    % MODULUS
+    ++ INCREMENT
+    -- DECREMENT
+*/
+
+//  COMPARISON OPERATORS
+/*
+    ==  EQUAL TO
+    === EQUAL VALUE AND EQUAL TYPE
+    !=  NOT EQUAL
+    !== NOT EQUAL VALUE OR NOT EQUAL TYPE
+    >   GREATER THAN
+    <   LESS THAN
+    >=  GREATER THAN OR EQUAL TO
+    <=  LESS THAN OR EQUAL TO
+    ?   TERNARY OPERATOR
+*/
+
+//  LOGICAL OPERATORS
+/*
+    &&  LOGICAL AND
+    ||  LOGICAL OR
+    !   LOGICAL NOT
+*/
